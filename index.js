7fb0d54738a4e7da3701a66f25912c7a9f1a954b
Joubert Saquett <jsaquett@haoc.com.br>
2021-03-12 16:52:22 -0300

    RESOLVED: day3 - start one

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 var day1 = require('./src/day_1');
 var day2 = require('./src/day_2');
+var day3 = require('./src/day_3');
 
-day1.day1();
-day2.exec();
+// day1.day1();
+// day2.exec();
+day3.exec();
